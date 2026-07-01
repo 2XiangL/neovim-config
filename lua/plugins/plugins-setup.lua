5bd1838bb6a5d5c2b6cc71b2b1cadb94076d06ba
2XiangL <nzgbsm@outlook.com>
2026-07-01 21:58:15 +0800

    remove claude code plugin

--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -99,31 +99,6 @@ local lspsaga = {
   'nvimdev/lspsaga.nvim'
 }
 
-local claude_code = {
-  "coder/claudecode.nvim",
-  dependencies = { "folke/snacks.nvim" },
-  config = true,
-  keys = {
-    { "<leader>c", nil, desc = "AI/Claude Code" },
-    { "<leader>cc", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
-    { "<leader>cf", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
-    { "<leader>cr", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
-    { "<leader>cC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
-    { "<leader>cm", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
-    { "<leader>cb", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
-    { "<leader>cs", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
-    {
-      "<leader>cs",
-      "<cmd>ClaudeCodeTreeAdd<cr>",
-      desc = "Add file",
-      ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
-    },
-    -- Diff management
-    { "<leader>ca", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
-    { "<leader>cd", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
-  },
-}
-
 local supermaven = {
   "supermaven-inc/supermaven-nvim",
   config = function()
@@ -147,6 +122,5 @@ require("lazy").setup({
   plugin_mason,
   plugin_nvim_cmp,
   lspsaga,
-  claude_code,
   supermaven,
 })
